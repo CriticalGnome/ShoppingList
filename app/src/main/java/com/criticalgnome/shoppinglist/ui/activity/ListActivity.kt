e6cbe4f435d7fa279d292f72cbe50ac7deb9da86
Sergey Kalinovsky <SergeyKalinovsky@coherentsolutions.com>
2018-10-09 10:49:00 +0300

    Fragments gone

--- app/src/main/java/com/criticalgnome/shoppinglist/ui/activity/ListActivity.kt
+++ app/src/main/java/com/criticalgnome/shoppinglist/ui/activity/ListActivity.kt
@@ -1,24 +1,43 @@
 package com.criticalgnome.shoppinglist.ui.activity
 
 import android.os.Bundle
+import android.view.View
 import com.criticalgnome.shoppinglist.R
-import com.criticalgnome.shoppinglist.ui.fragment.ItemFragment
-import com.criticalgnome.shoppinglist.ui.fragment.ListFragment
+import com.criticalgnome.shoppinglist.model.Pool
+import com.criticalgnome.shoppinglist.ui.adapter.ListItemAdapter
 import kotlinx.android.synthetic.main.activity_list.*
-import org.jetbrains.anko.toast
 
 class ListActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
-        fab.setOnClickListener {
-            toast("FAB button pressed")
-        }
-        showFragment(R.id.container, ListFragment())
 
-        fab.setOnClickListener {
-            showFragment(R.id.container, ItemFragment())
+        val items = Pool.items
+        items.addAll(Pool.items)
+        items.addAll(Pool.items)
+        val adapter = ListItemAdapter(items)
+        listRecycler.adapter = adapter
+
+        nameEditText.setOnFocusChangeListener { _, hasFocus -> setVisibilityOfAdditionalFields(hasFocus) }
+        descriptionEditText.setOnFocusChangeListener { _, hasFocus -> setVisibilityOfAdditionalFields(hasFocus) }
+        countEditText.setOnFocusChangeListener { _, hasFocus -> setVisibilityOfAdditionalFields(hasFocus) }
+        listRecycler.setOnTouchListener { _, _ ->
+            setVisibilityOfAdditionalFields(false)
+            return@setOnTouchListener false
+        }
+
+    }
+
+    private fun setVisibilityOfAdditionalFields(isVisible: Boolean) {
+        if (descriptionLayout.hasFocus() || countLayout.hasFocus()) return
+        descriptionLayout.visibility = if (isVisible) View.VISIBLE else View.GONE
+        countLayout.visibility = if (isVisible) View.VISIBLE else View.GONE
+        if (isVisible) {
+            descriptionLayout.bringToFront()
+            countLayout.bringToFront()
+        } else {
+
         }
     }
 }
